7fabe63e0e623d121c64d75c8496fecc4c863f48
Abhi Omar <abhiomar784@gmail.com>
2026-02-23 17:07:19 +0530

    Refactor array declaration in Day01.c

---
+++ Day01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n + 1];  
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos, x;
+
+    // Input position and element
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    // Convert 1-based position to 0-based index
+    int index = pos - 1;
+
+    // Shift elements to the right
+    for(int i = n; i > index; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert element
+    arr[index] = x;
+
+    // Print updated array
+    for(int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
