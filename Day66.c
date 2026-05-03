997812b30490bb9b63c905558ee067196d8028de
Abhi Omar <abhiomar784@gmail.com>
2026-05-03 21:46:36 +0530

    Create Day66.c

---
+++ Day66.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+int recStack[MAX];
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (directed)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+}
+
+// DFS function
+int dfs(int v) {
+    visited[v] = 1;
+    recStack[v] = 1;
+
+    struct Node* temp = adj[v];
+    while (temp) {
+        int next = temp->vertex;
+
+        if (!visited[next]) {
+            if (dfs(next))
+                return 1;
+        }
+        else if (recStack[next]) {
+            return 1; // cycle found
+        }
+
+        temp = temp->next;
+    }
+
+    recStack[v] = 0; // remove from recursion stack
+    return 0;
+}
+
+// Check cycle
+int hasCycle(int n) {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i))
+                return 1;
+        }
+    }
+    return 0;
+}
+
+// Main
+int main() {
+    int n, m;
+    scanf("%d", &n);  // vertices
+    scanf("%d", &m);  // edges
+
+    // Initialize
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+        recStack[i] = 0;
+    }
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    if (hasCycle(n))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
