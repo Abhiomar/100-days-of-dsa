64a0c722500e06a4b0fba5dd2f864af07a687c3f
Abhi Omar <abhiomar784@gmail.com>
2026-05-03 21:44:32 +0530

    Implement DFS for graph using adjacency list

---
+++ Day63.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (undirected)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    struct Node* newNode2 = createNode(u);
+    newNode2->next = adj[v];
+    adj[v] = newNode2;
+}
+
+// DFS function
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    struct Node* temp = adj[v];
+    while (temp) {
+        int next = temp->vertex;
+        if (!visited[next]) {
+            dfs(next);
+        }
+        temp = temp->next;
+    }
+}
+
+// Main
+int main() {
+    int n, m;
+    scanf("%d", &n);  // vertices
+    scanf("%d", &m);  // edges
+
+    // Initialize
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    int start;
+    scanf("%d", &start);
+
+    dfs(start);
+
+    return 0;
+}
